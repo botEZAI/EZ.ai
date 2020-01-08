c53aa492b31c6d8eb8cdaf3272d14a4e2e3ab8d3
HyeonJeong <hse05105@naver.com>
2020-01-09 01:21:09 +0900

    Chatbot merge 후 Directory 수정

--- ez.ai/src/components/ChatbotList/ChatbotList.js
+++ ez.ai/src/components/ChatbotList/ChatbotList.js
@@ -1,47 +1,51 @@
 import React from "react";
 import "./ChatbotList.css";
+import InputBot from './InputBot/InputBot';
 
 const searchResult = [
   {
-    id: 1,
-    loginName: "kakao Chatbot"
+      id: 1,
+      loginName: "kakao Chatbot"
   },
   {
-    id: 2,
-    loginName: "naver Chatbot"
+      id: 2,
+      loginName: "naver Chatbot"
   },
   {
-    id: 3,
-    loginName: "facebook Chatbot"
+      id: 3,
+      loginName: "Line Chatbot"
+  },
+  {
+      id: 4,
+      loginName: "facebook Chatbot"
   }
-];
+]
 
 const ChatbotList = () => {
   return (
-    <div class="main">
-      <div class="main-title">
-        <div class="sub-title">
-          <span class="list-title">나의 챗봇 목록</span>
+    <div>
+        <div className="main">
+        <div className="main-title">
+                <div className="sub-title">
+                    <span className="list-title">나의 챗봇 목록</span>
+                </div>
+                <div className="sub-title">
+                    <span>
+                        <input type="text" className="search-input" placeholder="검색어를 입력하세요."/>
+                    </span>
+                    <span className="search-btn-span">
+                        <button>검색</button>
+                    </span>
+                </div>
+            </div>
+            <div class="list-line"></div>   
+            <div className="content">
+                <InputBot />
+            </div>   
         </div>
-        <div class="sub-title">
-          <span>
-            <input
-              type="text"
-              class="search-input"
-              placeholder="검색어를 입력하세요."
-            />
-          </span>
-          <span class="search-btn-span">
-            <button>검색</button>
-          </span>
-        </div>
-      </div>
-      <div class="list-line"></div>
-      <div class="chat-list create-bot">
-        <span>+ 새로운 챗봇 만들기</span>
-      </div>
+
     </div>
-  );
+  )
 };
 
 export default ChatbotList;
