8bb766c4e53e4dbca96bb82a8e3d577e42024a1a
ChayanRin <k852012@naver.com>
2020-01-09 18:29:37 +0900

    Create Login basic component

--- ez.ai/src/components/Login.js
+++ ez.ai/src/components/Login.js
@@ -4,16 +4,21 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <>
+    <div id = "login">
+      <h1>Ez.ai</h1>
+      <p>Ez.ai에 오신 것을 환영합니다!</p>
+      <div>구글 로그인</div>
+      <div>네이버 로그인</div>
       <form className="login-form">
-        <input placeholder="이메일" />
+        <input placeholder="이메일" /><br />
         <input placeholder="비밀번호" />
+        <div>로그인 유지 아이디찾기 비밀번호찾기</div>
         <button>로그인</button>
         <Link to="/register">
           <button>회원가입</button>
         </Link>
       </form>
-    </>
+    </div>
   );
 };
 
