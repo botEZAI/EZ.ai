5f7ccbcef0aff2e57bd6ce8e63776f0b6d9fcd19
ChayanRin <k852012@naver.com>
2020-02-09 14:19:32 +0900

    Update Sidebar fold and animations

--- ez.ai/client/src/components/ChatbotBuild/BuilderNavContents/BuilderNav.js
+++ ez.ai/client/src/components/ChatbotBuild/BuilderNavContents/BuilderNav.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const BuilderNav = () => {
-    return (
-        <div className="builder__column builderNav">
-            <div className = "builderNav-btn">
-                <i className="fas fa-angle-double-right"></i>
-                <i className="fas fa-angle-double-left"></i>
-            </div>
-            <div className="preview">
+    const [navWidth, setNavWidth] = useState('400px');
+    const [leftArrowDisplay, setLeftArrowDisplay] = useState('none');
+    const [rightArrowDisplay, setRightArrowDisplay] = useState('block');
 
-            </div>
-        </div>
-    )
+
+    const navStyle = {
+        width : navWidth,
+        transition: '.5s width'
+    }
+    const rightArrow = {
+        display : rightArrowDisplay
+    }
+    const leftArrow = {
+        display : leftArrowDisplay
+    }
+
+    const foldNav = (e) => {
+        if (navWidth == '400px'){
+            setNavWidth('50px');
+            setLeftArrowDisplay('block');
+            setRightArrowDisplay('none');
+
+        } else {
+            setNavWidth('400px');
+            setLeftArrowDisplay('none');
+            setRightArrowDisplay('block');
+        }
+
+    }
+
+    return (
+            <cloasNav>
+                <div className="builder__column builderNav" style={navStyle}>
+                    <div className="builderNav-btn" onClick = {foldNav}>
+                        <i className="fas fa-angle-double-right" style={rightArrow}></i>
+                        <i className="fas fa-angle-double-left" style={leftArrow}></i>
+                    </div>
+                    <div className="preview">
+
+                    </div>
+                </div>
+            </cloasNav>
+        )
 }
 
 
