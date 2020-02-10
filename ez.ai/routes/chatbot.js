308e2445c7fc74639b09deb73a2b0be6e4dbcd35
junyeongjang <junyeong.dev@gmail.com>
2020-02-10 16:01:30 +0900

    Update chatbot.js
    챗봇 라우터 재구성

--- ez.ai/routes/chatbot.js
+++ ez.ai/routes/chatbot.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+const Chatbot = require('../models').Chatbot;
 router.post('/', function(req, res, next){
+    Chatbot.create({
+        keyword: req.body.keywordObject[0].keyword,
+        type: req.body.keywordObject[0].contents[0].type,
+        entity: req.body.keywordObject[0].contents[0].entity,
+        content: req.body.keywordObject[0].contents[0].content,
+      }).
+        then(() =>{
+          console.log("통과");
+        })
+        .catch((err)=>{
+          console.error(err);
+          next(err);
+        });
+
     console.log("받아지는 것 확인");
-    const content= req.body.keywordObject[0].contents;
+    const content= req.body.keywordObject[0].contents[0].type;
+ 
+    // // contents 내부 [ { type: 'text', id: 1, entity: '', content: 'gg' } ]
     console.log(content);
 });
 module.exports = router;
