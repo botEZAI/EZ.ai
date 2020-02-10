a4d0cdc0fb93b8c303e050dce8d9ff17b5d034c7
junyeongjang <junyeong.dev@gmail.com>
2020-02-10 16:06:00 +0900

    Update server.js
    메인 서버 코드 수정

--- ez.ai/server.js
+++ ez.ai/server.js
@@ -1,9 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = process.env.PORT || 5000;
+var express = require("express");
+var bodyParser = require("body-parser");
+var port = process.env.PORT || 5000;
+var cookieParser = require('cookie-parser');
+var sequelize = require('./models').sequelize;
+
+var app = express();
+sequelize.sync();
+
+//라우터 연결
+var chatbotRouter = require('./routes/chatbot');
+
+//미들웨어 사용
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+//라우터 사용
+
+app.use('/api/chatbotbuild',chatbotRouter);
+
 
 app.get("api/register", (req, res) => {
   res.send({ message: "Hello Express!" });
@@ -14,7 +31,9 @@ app.post("/api/register", function(req, res) {
   res.send("POST request to the homepage");
 });
 
-app.post("/api/chatbotbuild", (req, res) => {
-  res.send({ message: "data post" });
-});
+// app.post("/api/chatbotbuild", (req, res) => {
+//   console.log("받아지는 것 확인");
+//   const content= req.body.keywordObject[0].keyword;
+//   console.log(content);
+// });
 app.listen(port, () => console.log(`Listening on port ${port}`));
