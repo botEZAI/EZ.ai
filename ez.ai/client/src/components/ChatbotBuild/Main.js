d2ac7c408db531743347304c35d179edfbc21915
ChayanRin <k852012@naver.com>
2020-02-14 19:08:48 +0900

    Merge branch 'master' of https://github.com/Hanswind/EZ.ai

--- ez.ai/client/src/components/ChatbotBuild/Main.js
+++ ez.ai/client/src/components/ChatbotBuild/Main.js
@@ -36,7 +36,7 @@ const Main = ({
           v.type === "text" ? (
             <>
               <div className="main-content" key={v.contnet + i}>
-                <input
+                <textarea
                   onClick={e => onClickCurrent(e)}
                   value={v.content || ""}
                   onChange={e => {
@@ -49,7 +49,7 @@ const Main = ({
                       })
                     );
                   }}
-                  placeholder="text"
+                  placeholder="작성하고자 하는 텍스트를 적어주세요"
                 />
               </div>
             </>
