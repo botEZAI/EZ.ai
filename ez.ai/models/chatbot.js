2a74f543016135bd640c738b36c0f5fbf5d43f10
junyeongjang <junyeong.dev@gmail.com>
2020-02-17 22:13:14 +0900

    Update chatbot.js
    챗봇 모델 재구성

--- ez.ai/models/chatbot.js
+++ ez.ai/models/chatbot.js
@@ -1,10 +1,5 @@
 module.exports = (sequelize, DataTypes) => (
     sequelize.define('chatbot', {
-      keyword: {
-        type: DataTypes.STRING(200),
-        allowNull: false,
-        unique: true,
-      },
       type: {
         type: DataTypes.STRING(20),
         allowNull: false,
