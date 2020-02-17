44e0d058c0a634e31a57fd1fe45f5033edef9a93
CHOIKEONWOO <easter9211@gmail.com>
2020-02-17 23:41:15 +0900

    Sync between status and main

--- ez.ai/client/src/components/ChatbotBuild/Main.js
+++ ez.ai/client/src/components/ChatbotBuild/Main.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import "./Main.css";
 import GoogleMapPresenter from "./GoogleMapPresenter";
 
-
 const Main = ({
   mainKeyword,
   keywordContentList,
@@ -26,11 +25,17 @@ const Main = ({
     <>
       <div className="main-header">
         <div className="main-header-icon">
-          <span className="item"><i className="fa fa-arrow-left"></i></span>
-          <span className="item"><i className="fa fa-user-circle fa-2x"></i></span>
+          <span className="item">
+            <i className="fa fa-arrow-left"></i>
+          </span>
+          <span className="item">
+            <i className="fa fa-user-circle fa-2x"></i>
+          </span>
           <span className="item">USER</span>
         </div>
-        <div className="item-last"><i className="fa fa-ellipsis-v"></i></div>
+        <div className="item-last">
+          <i className="fa fa-ellipsis-v"></i>
+        </div>
       </div>
       <div className="main-contents">
         {keywordObject[index] && (
@@ -39,208 +44,210 @@ const Main = ({
           </div>
         )}
         {keywordObject[index] &&
-        keywordObject[index].contents.map((v, i) =>
-          v.type === "text" ? (
-            <>
-              <div
-                className="main-content"
-                onClick={() => setClickedMainInput(v)}
-                key={v.contnet + i}
-              >
-                <textarea
-                  value={v.content || ""}
-                  onChange={e => {
-                    setKeywordObject(
-                      produce(keywordObject, draft => {
-                        const tmp = draft[index].contents.find(
-                          t => t.id === v.id
-                        );
-                        tmp.content = e.target.value;
-                      })
-                    );
-                  }}
-                  placeholder="작성하고자 하는 텍스트를 적어주세요"
-                />
-              </div>
-            </>
-          ) : v.type === "image" ? (
-            <>
-              <div className="main-content" key={v.contnet + i}>
-                <input
-                  value={v.content || ""}
-                  onChange={e => {
-                    setKeywordObject(
-                      produce(keywordObject, draft => {
-                        const tmp = draft[index].contents.find(
-                          t => t.id === v.id
-                        );
-                        tmp.content = e.target.value;
-                      })
-                    );
-                  }}
-                  placeholder="image url"
-                />
-                <div className = "image-preview">
-                  <div className = "image-preview-screen">이미지 미리보기</div>
+          keywordObject[index].contents.map((v, i) =>
+            v.type === "text" ? (
+              <>
+                <div
+                  className="main-content"
+                  onClick={() => setClickedMainInput(v)}
+                  key={v.contnet + i}
+                >
+                  <textarea
+                    value={v.content || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          const tmp = draft[index].contents.find(
+                            t => t.id === v.id
+                          );
+                          tmp.content = e.target.value;
+                        })
+                      );
+                    }}
+                    placeholder="작성하고자 하는 텍스트를 적어주세요"
+                  />
                 </div>
-              </div>
-            </>
-          ) : v.type === "location" ? (
-            <>
-              <div className="main-content" key={v.contnet + i}>
-                <input
-                  value={v.content.title || ""}
-                  onChange={e => {
-                    setKeywordObject(
-                      produce(keywordObject, draft => {
-                        const tmp = draft[index].contents.find(
-                          t => t.id === v.id
+              </>
+            ) : v.type === "image" ? (
+              <>
+                <div
+                  className="main-content"
+                  onClick={() => setClickedMainInput(v)}
+                  key={v.contnet + i}
+                >
+                  <input
+                    value={v.content || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          const tmp = draft[index].contents.find(
+                            t => t.id === v.id
+                          );
+                          tmp.content = e.target.value;
+                        })
+                      );
+                    }}
+                    placeholder="image url"
+                  />
+                  <div className="image-preview">
+                    <div className="image-preview-screen">이미지 미리보기</div>
+                  </div>
+                </div>
+              </>
+            ) : v.type === "location" ? (
+              <>
+                <div className="main-content" key={v.contnet + i}>
+                  <input
+                    value={v.content.title || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          const tmp = draft[index].contents.find(
+                            t => t.id === v.id
+                          );
+                          tmp.content.title = e.target.value;
+                        })
+                      );
+                    }}
+                    placeholder="title"
+                  />
+                  <input
+                    value={v.content.latitude || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          const tmp = draft[index].contents.find(
+                            t => t.id === v.id
+                          );
+                          tmp.content.latitude = e.target.value;
+                        })
+                      );
+                    }}
+                    placeholder="latitude"
+                  />
+                  <input
+                    value={v.content.longtitude || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          const tmp = draft[index].contents.find(
+                            t => t.id === v.id
+                          );
+                          tmp.content.longtitude = e.target.value;
+                        })
+                      );
+                    }}
+                    placeholder="longtitude"
+                  />
+                  <GoogleMapPresenter />
+                </div>
+              </>
+            ) : v.type === "list" ? (
+              <>
+                <div className="main-content list-content" key={v.contnet + i}>
+                  <input
+                    value={v.content[0] || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          const tmp = draft[index].contents.find(
+                            t => t.id === v.id
+                          );
+                          tmp.content[0] = e.target.value;
+                        })
+                      );
+                    }}
+                    placeholder="list"
+                  />
+
+                  {v.content[1] && (
+                    <input
+                      value={v.content[1] || ""}
+                      onChange={e => {
+                        setKeywordObject(
+                          produce(keywordObject, draft => {
+                            const tmp = draft[index].contents.find(
+                              t => t.id === v.id
+                            );
+                            tmp.content[1] = e.target.value;
+                          })
                         );
-                        tmp.content.title = e.target.value;
-                      })
-                    );
-                  }}
-                  placeholder="title"
-                />
-                <input
-                  value={v.content.latitude || ""}
-                  onChange={e => {
-                    setKeywordObject(
-                      produce(keywordObject, draft => {
-                        const tmp = draft[index].contents.find(
-                          t => t.id === v.id
+                      }}
+                      placeholder="list"
+                    />
+                  )}
+
+                  {v.content[2] && (
+                    <input
+                      value={v.content[2] || ""}
+                      onChange={e => {
+                        setKeywordObject(
+                          produce(keywordObject, draft => {
+                            const tmp = draft[index].contents.find(
+                              t => t.id === v.id
+                            );
+                            tmp.content[2] = e.target.value;
+                          })
                         );
-                        tmp.content.latitude = e.target.value;
-                      })
-                    );
-                  }}
-                  placeholder="latitude"
-                />
-                <input
-                  value={v.content.longtitude || ""}
-                  onChange={e => {
-                    setKeywordObject(
-                      produce(keywordObject, draft => {
-                        const tmp = draft[index].contents.find(
-                          t => t.id === v.id
+                      }}
+                      placeholder="list"
+                    />
+                  )}
+
+                  {v.content[3] && (
+                    <input
+                      value={v.content[3] || ""}
+                      onChange={e => {
+                        setKeywordObject(
+                          produce(keywordObject, draft => {
+                            const tmp = draft[index].contents.find(
+                              t => t.id === v.id
+                            );
+                            tmp.content[3] = e.target.value;
+                          })
                         );
-                        tmp.content.longtitude = e.target.value;
-                      })
-                    );
-                  }}
-                  placeholder="longtitude"
-                />
-                <GoogleMapPresenter />
-              </div>
-              
-              
-            </>
-          ) : v.type === "list" ? (
-            <>
-              <div className="main-content list-content" key={v.contnet + i}>
-                <input
-                  value={v.content[0] || ""}
-                  onChange={e => {
-                    setKeywordObject(
-                      produce(keywordObject, draft => {
-                        const tmp = draft[index].contents.find(
-                          t => t.id === v.id
+                      }}
+                      placeholder="list"
+                    />
+                  )}
+
+                  {v.content[4] && (
+                    <input
+                      value={v.content[4] || ""}
+                      onChange={e => {
+                        setKeywordObject(
+                          produce(keywordObject, draft => {
+                            const tmp = draft[index].contents.find(
+                              t => t.id === v.id
+                            );
+                            tmp.content[4] = e.target.value;
+                          })
                         );
-                        tmp.content[0] = e.target.value;
-                      })
-                    );
-                  }}
-                  placeholder="list"
-                />
+                      }}
+                      placeholder="list"
+                    />
+                  )}
 
-                {v.content[1] && (
-                  <input
-                    value={v.content[1] || ""}
-                    onChange={e => {
-                      setKeywordObject(
-                        produce(keywordObject, draft => {
-                          const tmp = draft[index].contents.find(
-                            t => t.id === v.id
-                          );
-                          tmp.content[1] = e.target.value;
-                        })
-                      );
-                    }}
-                    placeholder="list"
-                  />
-                )}
-
-                {v.content[2] && (
-                  <input
-                    value={v.content[2] || ""}
-                    onChange={e => {
-                      setKeywordObject(
-                        produce(keywordObject, draft => {
-                          const tmp = draft[index].contents.find(
-                            t => t.id === v.id
-                          );
-                          tmp.content[2] = e.target.value;
-                        })
-                      );
-                    }}
-                    placeholder="list"
-                  />
-                )}
-
-                {v.content[3] && (
-                  <input
-                    value={v.content[3] || ""}
-                    onChange={e => {
-                      setKeywordObject(
-                        produce(keywordObject, draft => {
-                          const tmp = draft[index].contents.find(
-                            t => t.id === v.id
-                          );
-                          tmp.content[3] = e.target.value;
-                        })
-                      );
-                    }}
-                    placeholder="list"
-                  />
-                )}
-
-                {v.content[4] && (
-                  <input
-                    value={v.content[4] || ""}
-                    onChange={e => {
-                      setKeywordObject(
-                        produce(keywordObject, draft => {
-                          const tmp = draft[index].contents.find(
-                            t => t.id === v.id
-                          );
-                          tmp.content[4] = e.target.value;
-                        })
-                      );
-                    }}
-                    placeholder="list"
-                  />
-                )}
-
-                {v.content[5] && (
-                  <input
-                    value={v.content[5] || ""}
-                    onChange={e => {
-                      setKeywordObject(
-                        produce(keywordObject, draft => {
-                          const tmp = draft[index].contents.find(
-                            t => t.id === v.id
-                          );
-                          tmp.content[5] = e.target.value;
-                        })
-                      );
-                    }}
-                    placeholder="list"
-                  />
-                )}
-              </div>
-            </>
-          ) : null
-        )}
+                  {v.content[5] && (
+                    <input
+                      value={v.content[5] || ""}
+                      onChange={e => {
+                        setKeywordObject(
+                          produce(keywordObject, draft => {
+                            const tmp = draft[index].contents.find(
+                              t => t.id === v.id
+                            );
+                            tmp.content[5] = e.target.value;
+                          })
+                        );
+                      }}
+                      placeholder="list"
+                    />
+                  )}
+                </div>
+              </>
+            ) : null
+          )}
       </div>
       <div className="main-footer">
         <button className="main-button" onClick={onClickButton}>
--- ez.ai/client/src/components/ChatbotBuild/ToolStatus.js
+++ ez.ai/client/src/components/ChatbotBuild/ToolStatus.js
@@ -16,35 +16,60 @@ const ToolStatus = ({
     keywordObject[index] &&
     keywordObject[index].contents[length - 1] &&
     keywordObject[index].contents[length - 1].content;
-  console.log(clickedMainInput.content);
+  const clickedIndex =
+    keywordObject[index] &&
+    keywordObject[index].contents.findIndex(v => v.id === clickedMainInput.id);
+  console.log(
+    "clicked=",
+    keywordObject[index] &&
+      keywordObject[index].contents[clickedIndex] &&
+      keywordObject[index].contents[clickedIndex].content
+  );
+
   return (
     <>
-    
-    
-            <div className = "tool-status-header">
-            <div className="tool-status-name">
-                {currentInput &&
-                    (currentInput.type === "text" ? (
-                        <>
-                            <span>텍스트</span>
-                        </>
-                    ) : currentInput.type === "image" ? (
-                        <>
-                            <span>이미지</span>
-                        </>
-                        ) : currentInput.type === "location" ? (
-                        <>
-                            <span>위치</span>
-                        </>
-                        ) : currentInput.type === "list" ? (
-                        <>
-                            <span>버튼형 리스트</span>
-                        </>
-                        ) : null)}
-            </div>
-            <div className = "help" alt="도움말">?</div>
+      <div className="tool-status-header">
+        <div className="tool-status-name">
+          {currentInput && !clickedMainInput.content ? (
+            currentInput.type === "text" ? (
+              <>
+                <span>텍스트</span>
+              </>
+            ) : currentInput.type === "image" ? (
+              <>
+                <span>이미지</span>
+              </>
+            ) : currentInput.type === "location" ? (
+              <>
+                <span>위치</span>
+              </>
+            ) : currentInput.type === "list" ? (
+              <>
+                <span>버튼형 리스트</span>
+              </>
+            ) : null
+          ) : clickedMainInput.type === "text" ? (
+            <>
+              <span>텍스트</span>
+            </>
+          ) : clickedMainInput.type === "image" ? (
+            <>
+              <span>이미지</span>
+            </>
+          ) : clickedMainInput.type === "location" ? (
+            <>
+              <span>위치</span>
+            </>
+          ) : clickedMainInput.type === "list" ? (
+            <>
+              <span>버튼형 리스트</span>
+            </>
+          ) : null}
         </div>
-
+        <div className="help" alt="도움말">
+          ?
+        </div>
+      </div>
 
       <div className="tool-status-main">
         {!clickedMainInput.content &&
@@ -67,39 +92,41 @@ const ToolStatus = ({
               </div>
             </>
           ) : currentInput.type === "image" ? (
-            <div className ="image-status">
-                <div className="status-image-tab">
-                    <div className = "image-tab-btn outer-img-link">외부 이미지 URL</div>
-                    <div className = "image-tab-btn upload-img-file">이미지 첨부하기</div>
+            <div className="image-status">
+              <div className="status-image-tab">
+                <div className="image-tab-btn outer-img-link">
+                  외부 이미지 URL
                 </div>
+                <div className="image-tab-btn upload-img-file">
+                  이미지 첨부하기
+                </div>
+              </div>
 
-                <div className="status-input status-image">
-                    <div className = "status-image-input">
-                        <input
-                          placeholder="외부 URL를 입력해주세요"
-                          value={currentContent || ""}
-                          onChange={e => {
-                            setKeywordObject(
-                              produce(keywordObject, draft => {
-                                draft[index].contents[length - 1].content =
-                                  e.target.value;
-                              })
-                            );
-                          }}
-                        />
-                        <div className="outer-img-btn">적용</div>
-                    </div>
-                    <div className = "image-preview">
-                        <div className = "image-preview-screen">이미지 미리보기</div>
-                    </div>
+              <div className="status-input status-image">
+                <div className="status-image-input">
+                  <input
+                    placeholder="외부 URL를 입력해주세요"
+                    value={currentContent || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          draft[index].contents[length - 1].content =
+                            e.target.value;
+                        })
+                      );
+                    }}
+                  />
+                  <div className="outer-img-btn">적용</div>
                 </div>
+                <div className="image-preview">
+                  <div className="image-preview-screen">이미지 미리보기</div>
+                </div>
+              </div>
             </div>
-
           ) : currentInput.type === "location" ? (
             <>
-
               <div className="status-input status-location">
-                  <p>임시 - 추후 지도 API로 연동</p>
+                <p>임시 - 추후 지도 API로 연동</p>
                 <input
                   placeholder="장소 이름을 적어주세요"
                   value={currentContent.title || ""}
@@ -137,13 +164,16 @@ const ToolStatus = ({
                   }}
                 />
               </div>
-                <GoogleMapPresenter />;
+              <GoogleMapPresenter />;
             </>
           ) : currentInput.type === "list" ? (
             <>
               <div className="status-input status-list">
                 <table>
-                    <p>텍스트작성 / 키워드 등록(키워드에 대한 텍스트 작성) / 마지막요소로 권장 등 추가하기</p>
+                  <p>
+                    텍스트작성 / 키워드 등록(키워드에 대한 텍스트 작성) /
+                    마지막요소로 권장 등 추가하기
+                  </p>
                   <tr>
                     <input
                       placeholder="키워드명을 적어주세요"
@@ -229,24 +259,66 @@ const ToolStatus = ({
           (clickedMainInput.type === "text" ? (
             <>
               <div className="status-input status-text">
-                <input value={clickedMainInput.content} />
+                <textarea
+                  placeholder="작성하고자 하는 텍스트를 적어주세요"
+                  value={
+                    keywordObject[index].contents[clickedIndex].content || ""
+                  }
+                  onChange={e => {
+                    setKeywordObject(
+                      produce(keywordObject, draft => {
+                        draft[index].contents[clickedIndex].content =
+                          e.target.value;
+                      })
+                    );
+                  }}
+                />
               </div>
             </>
+          ) : clickedMainInput.type === "image" ? (
+            <div className="image-status">
+              <div className="status-image-tab">
+                <div className="image-tab-btn outer-img-link">
+                  외부 이미지 URL
+                </div>
+                <div className="image-tab-btn upload-img-file">
+                  이미지 첨부하기
+                </div>
+              </div>
+
+              <div className="status-input status-image">
+                <div className="status-image-input">
+                  <input
+                    placeholder="외부 URL를 입력해주세요"
+                    value={currentContent || ""}
+                    onChange={e => {
+                      setKeywordObject(
+                        produce(keywordObject, draft => {
+                          draft[index].contents[length - 1].content =
+                            e.target.value;
+                        })
+                      );
+                    }}
+                  />
+                  <div className="outer-img-btn">적용</div>
+                </div>
+                <div className="image-preview">
+                  <div className="image-preview-screen">이미지 미리보기</div>
+                </div>
+              </div>
+            </div>
           ) : null)}
       </div>
 
-
-
-        <div className = "tool-status-nav">
-            <div></div>
-            <div className = "tool-status-nav-btns">
-                <div className = "tool-status-btn confirm">확인</div>
-                <div className = "tool-status-btn decline">삭제</div>
-            </div>
+      <div className="tool-status-nav">
+        <div></div>
+        <div className="tool-status-nav-btns">
+          <div className="tool-status-btn confirm">확인</div>
+          <div className="tool-status-btn decline">삭제</div>
         </div>
+      </div>
     </>
   );
 };
 
 export default ToolStatus;
-
