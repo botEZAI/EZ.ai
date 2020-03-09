4adde33a05ebed0f7f67936f02a09dae2445b4d3
junyeongjang <junyeong.dev@gmail.com>
2020-03-09 19:50:42 +0900

    Update chatbot.js
    키워드 + 콘텐츠 저장 부분 수정 중

--- ez.ai/routes/chatbot.js
+++ ez.ai/routes/chatbot.js
@@ -3,46 +3,66 @@ var router = express.Router();
 const Chatbot = require('../models').Chatbot;
 const Keyword = require('../models').Keyword;
 router.post('/', function(req, res, next){
-  console.log(req.body);
-  console.log(req.body.k);
-  const keyword = req.body.k.keyword;
+  const post_keyword = req.body.k.keyword;
   const keyword_id = req.body.k.id;
   const contents = req.body.k.contents;
   
   //console.log(contents.length);
-  console.log(keyword);
-  console.log(contents);
-  Keyword.create({
-    keyword: keyword,
+
+  if(post_keyword){
+    console.log(post_keyword);
+    console.log(contents);
+    Keyword.create({
+      keyword: post_keyword,
     })
     .then(() =>{
       console.log("keyword 통과");
+      for(var i=0;i<contents.length;i++){
+        Chatbot.create({
+          keyworder: keyword_id,
+          type: contents[i].type,
+          content: contents[i].content,
+        }).
+        then(() =>{
+          console.log("content 통과");
+        }).
+          catch((err)=>{
+            console.error(err);
+            next(err);
+        });
+      } 
     })
     .catch((err)=>{
       console.error(err);
       next(err);
     });
+  }
     
-  if(contents){
-  for(var i=0;i<contents.length;i++){
-  Chatbot.create({
-    keyworder: keyword_id,
-    type: contents[i].type,
-    content: contents[i].content,
-    //location에 대한 정보 아직 보류 
-    // title : contents[i].content[0].title,
-    // latitude : contents[i].content[1].latitude,
-    // longtitude: contents[i].content[2].longtitude,
-    }).
-      then(() =>{
-        console.log("content 통과");
-      })
-      .catch((err)=>{
-        console.error(err);
-        next(err);
-    });  
-  }     
-}
-
+    // for(var i=0;i<contents.length;i++){
+    //   Keyword.findOne({
+    //     where:{
+    //       keyword: post_keyword,
+    //     },
+    //   })
+    //    .then((keywords)=>{
+    //       Chatbot.create({
+    //         keyworder: keywords.id,
+    //         type: contents[i].type,
+    //         content: contents[i].content,
+    //         //location에 대한 정보 , 정보 입력 불가 
+    //         // title : contents[i].title,
+    //         // latitude : contents[i].latitude,
+    //         // longtitude: contents[i].longtitude,
+    //       }).
+    //         then(() =>{
+    //           console.log("content 통과");
+    //         }).
+    //           catch((err)=>{
+    //             console.error(err);
+    //             next(err);
+    //            });
+    //     });
+    //   }
+   
 });
 module.exports = router;
