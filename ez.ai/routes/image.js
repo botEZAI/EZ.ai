d70da7c2524ee0abc19598a026d14540d027c7b3
junyeongjang <junyeong.dev@gmail.com>
2020-03-10 11:44:36 +0900

    Update image.js
    이미지 제한 용량 설정

--- ez.ai/routes/image.js
+++ ez.ai/routes/image.js
@@ -14,7 +14,7 @@ const upload = multer({
             cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext);
         },
     }),
-    limits: {fileSize: 5 * 1024 * 1024},
+    limits: {fileSize: 30 * 1024 * 1024},
 });
 
 
