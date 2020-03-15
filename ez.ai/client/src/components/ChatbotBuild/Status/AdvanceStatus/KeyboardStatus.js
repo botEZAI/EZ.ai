56b86bfa0d3e1c99ef9fc93c02f61d87dff43155
CHOIKEONWOO <easter9211@gmail.com>
2020-03-15 13:01:37 +0900

    Change data structure
    keywordLink 추가

--- ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/KeyboardStatus.js
+++ ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/KeyboardStatus.js
@@ -1,78 +1,73 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import produce from "immer";
 import KeywordPopUp from "./KeywordPopUp";
 
-
-
 const KeyboardStatus = ({
-    currentContent,
-    setKeywordObject,
-    keywordObject,
-    now,
-    index,
-    keywordPopup,
-    setKeywordPopup,
+  currentContent,
+  setKeywordObject,
+  keywordObject,
+  now,
+  index,
+  keywordPopup,
+  setKeywordPopup
 }) => {
-    const [showPopup, setShowPopup] = useState(false);
-    const [popNum, setPopNum] = useState("-1");
+  const [showPopup, setShowPopup] = useState(false);
+  const [popNum, setPopNum] = useState("-1");
 
-    const toggleKeywordPopUp = e => {
-        setPopNum(e.target.getAttribute("name"));
-        setShowPopup(!showPopup)
-    };
+  const toggleKeywordPopUp = (i, e) => {
+    setPopNum(i);
+    setShowPopup(!showPopup);
+  };
 
-
-    return (
-        <>
-            <div className="status-input status-list">
-                <textarea
-                    placeholder="작성하고자 하는 텍스트를 적어주세요"
-                    value={currentContent.question || ""}
-                    onChange={e => {
-                        setKeywordObject(
-                            produce(keywordObject, draft => {
-                                draft[index].contents[now].content.question =
-                                    e.target.value;
-                            })
-                        );
-                    }}
-                ></textarea>
-                <table>
-                    {keywordPopup.map((keywordPop, i) => (
-                        <div className="status-list-content">
-                            <input
-                                placeholder="키워드명을 적어주세요"
-                                value={currentContent.elem[i] || ""}
-                                onChange={e => {
-                                    setKeywordObject(
-                                        produce(keywordObject, draft => {
-                                            draft[index].contents[now].content.elem[i] =
-                                                e.target.value;
-                                        })
-                                    );
-                                }}
-                            />
-                            <div
-                                className="list-keyword-btn"
-                                name = {keywordPop.id}
-                                onClick={toggleKeywordPopUp}
-                            >
-                                {keywordPop.value}
-                                {showPopup && popNum === keywordPop.id.toString() ?
-                                    <KeywordPopUp
-                                        keywordObject = {keywordObject}
-                                        keywordPopup = {keywordPopup}
-                                        setKeywordPopup = {setKeywordPopup}
-                                        keywordPop={keywordPop}
-                                    />
-                                    : null}
-                            </div>
-                        </div>
-                    ))}
-                </table>
+  return (
+    <>
+      <div className="status-input status-list">
+        <textarea
+          placeholder="작성하고자 하는 텍스트를 적어주세요"
+          value={currentContent.question || ""}
+          onChange={e => {
+            setKeywordObject(
+              produce(keywordObject, draft => {
+                draft[index].contents[now].content.question = e.target.value;
+              })
+            );
+          }}
+        ></textarea>
+        <table>
+          {currentContent.elem.map((e, i) => (
+            <div className="status-list-content">
+              <input
+                placeholder="키워드명을 적어주세요"
+                value={currentContent.elem[i] || ""}
+                onChange={e => {
+                  setKeywordObject(
+                    produce(keywordObject, draft => {
+                      draft[index].contents[now].content.elem[i] =
+                        e.target.value;
+                    })
+                  );
+                }}
+              />
+              <div
+                className="list-keyword-btn"
+                name={i}
+                onClick={() => toggleKeywordPopUp(i)}
+              >
+                {currentContent.keywordLink[i] || "연동"}
+                {showPopup && popNum === i ? (
+                  <KeywordPopUp
+                    keywordObject={keywordObject}
+                    currentContent={currentContent}
+                    currentElemIndex={i}
+                  />
+                ) : null}
+              </div>
             </div>
-        </>
-    )
-}
+          ))}
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default KeyboardStatus;
--- ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/KeywordPopUp.js
+++ ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/KeywordPopUp.js
@@ -1,43 +1,33 @@
 import React from "react";
 import "./KeywordPopUp.css";
 
+const KeywordPopUp = ({ keywordObject, currentContent, currentElemIndex }) => {
+  const setListKeyword = e => {
+    let newKeywordPopup = keywordPopup;
+    newKeywordPopup[keywordPop.id] = {
+      id: keywordPop.id,
+      value: e.target.getAttribute("label")
+    };
+    setKeywordPopup(newKeywordPopup);
+  };
 
-const KeywordPopUp = ({
-    keywordObject,
-    keywordPopup,
-    setKeywordPopup,
-    keywordPop,
-}) => {
-    const setListKeyword = (e) => {
-        let newKeywordPopup = keywordPopup;
-        newKeywordPopup[keywordPop.id] = {id : keywordPop.id, value: e.target.getAttribute('label')};
-        setKeywordPopup(newKeywordPopup);
-    }
-
-    return (
-        <div className="keyword-popup">
-            <div className="keyword-name">
-                keyword 목록
-            </div>
-            {keywordObject.map((keyword, index) => {
-                return (
-                <div
-                    key={index}
-                    label = {keyword.keyword}
-                    onClick={setListKeyword}
-                    className="keyword-list"
-                >
-                    {keyword.keyword}
-                </div>
-                );
-            })}
-        </div>
-    )
-}
-
-
-
-
-
+  return (
+    <div className="keyword-popup">
+      <div className="keyword-name">keyword 목록</div>
+      {keywordObject.map((keyword, index) => {
+        return (
+          <div
+            key={index}
+            label={keyword.keyword}
+            onClick={setListKeyword}
+            className="keyword-list"
+          >
+            {keyword.keyword}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default KeywordPopUp;
