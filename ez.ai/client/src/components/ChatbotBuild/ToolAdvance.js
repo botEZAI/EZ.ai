3d5c141951a514c4bae79cf5c29e78ef7a5f1d01
giraff <hse05105@naver.com>
2020-03-21 21:17:18 +0900

    Add func 'alertForKeywordCompleted' and state 'keywordCompleted'
    - 한 키워드에 버튼형 리스트 0개 아니면 1개이며, 버튼형 리스트 생성한 후에는 다른 버튼들이 눌리지 않도록 alert 띄움.
    - (사소한 것) 작업 중 쉽게 비교, 분석하기 위해 컴포넌트 props들 알파벳 순으로 정리한 것 뿐. 별 의미 없음.
    - (ToolBasic.js , ToolAdvance.js : onClick 내용이 복잡해져서 handleClick 함수로 간결하게 정리)

--- ez.ai/client/src/components/ChatbotBuild/ToolAdvance.js
+++ ez.ai/client/src/components/ChatbotBuild/ToolAdvance.js
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import produce from "immer";
 
 const ToolAdvance = ({
+  keywordCompleted,
+  keywordObject,
   mainKeyword,
   onClickBasic,
-  keywordObject,
-  setKeywordObject,
-  setClickedMainInput,
   setAddFlag,
-  setVirtualKeyboard,
+  setClickedMainInput,
+  setKeywordCompleted,
+  setKeywordObject,
   setNow,
-  length
+  setVirtualKeyboard,
+  length,
 }) => {
   const onClickTool = tool => {
     console.log("onClickTool");
@@ -35,31 +37,36 @@ const ToolAdvance = ({
                 });
             setAddFlag(true); // 컨텐츠 추가됨 => 스크롤 하단으로 이동
             setVirtualKeyboard(true); // 새 '리스트' 생성 시, Main 에서 바로 하단 바 보이게 함
+            setKeywordCompleted(true);
           })
         )
       : alert("키워드를 선택하세요");
     setClickedMainInput("");
     console.log(keywordObject);
   };
+  const AlertForKeywordCompleted = () => {
+    alert(`[고급] > [버튼형 리스트] 생성 후, 어떤 요소도 추가할 수 없습니다. \n요소 추가를 원하시면 생성한 [버튼형 리스트]를 삭제하세요.`);
+  }
+  const handleClick = tool => {
+    keywordCompleted 
+      ? AlertForKeywordCompleted()
+      : 
+        onClickTool(tool);
+        setNow(length + 1);
+  }
   return (
     <>
       {console.log("Tooladvance")}
       <div
         className="tool-basic tool-basic-text"
-        onClick={() => {
-          onClickTool("list");
-          setNow(length + 1);
-        }}
+        onClick={() => handleClick("list")}
       >
         <i className="fas fa-list"></i>
         버튼형 리스트
       </div>
       <div
         className="tool-basic tool-basic-text"
-        onClick={() => {
-          onClickTool("sticker");
-          setNow(length + 1);
-        }}
+        onClick={() => handleClick("sticker")}
       >
         <i className="far fa-laugh"></i>
         스티커
