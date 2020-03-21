3ebfacc23052042b267826a5757610d48e496da1
junyeongjang <junyeong.dev@gmail.com>
2020-03-22 00:04:33 +0900

    Update chatbot.js
    리스트 추가

--- ez.ai/routes/chatbot.js
+++ ez.ai/routes/chatbot.js
@@ -3,15 +3,21 @@ var router = express.Router();
 const Chatbot = require('../models').Chatbot;
 const Keyword = require('../models').Keyword;
 router.post('/', function(req, res, next){
-
   const post_keyword = req.body.nowKeyword.keyword;
   // const keyword_id = req.body.nowKeyword.keyword.id; DB조회 후 키에 넣기 떄문에 필요 x
   const contents = req.body.nowKeyword.contents;
-  //console.log(contents[0].content.elem);
+  //리스트
 
+  // {
+  // type: 'list',
+  // id: 1,
+  // listContent: { question: '1', elem: [Array], keywordLink: [Array] }
+  // }
+
+  console.log(contents[0].listContent);
   if(post_keyword){
-    console.log(post_keyword);
-    console.log(contents);
+    // console.log(post_keyword);
+    // console.log(contents);
     //리스트 추가 작업 중
     Keyword.create({
       keyword: post_keyword,
@@ -30,12 +36,16 @@ router.post('/', function(req, res, next){
           longtitude: contents[i].longtitude,
           // 카테고리 (추후 추가 예정)
           // kategorie: contents[i].kategorie,
-          
-          //리스트 정보
 
-          //개수
+          //리스트 정보
+          question: contents[i].listContent.question,
           //내용 1
-          //...f
+          elem1: contents[i].listContent.elem[0],
+          elem2: contents[i].listContent.elem[1],
+          elem3: contents[i].listContent.elem[2],
+          elem4: contents[i].listContent.elem[3],
+          elem5: contents[i].listContent.elem[4],
+          elem6: contents[i].listContent.elem[5],
           //~내용 5
         }).
         then(() =>{
