06f02c607b7e0e1a3bbc8379b83c20760fd2ceaf
ChayanRin <k852012@naver.com>
2020-04-04 16:11:43 +0900

    Add footer section

--- ez.ai/client/src/components/Footer.js
+++ ez.ai/client/src/components/Footer.js
@@ -3,10 +3,9 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <></>
-    /*<footer>
-
-        </footer>*/
+    <footer>
+      <p>Copyright 2020 ⓒ Team Ez.ai All Rights Reserved.</p>
+    </footer>
   );
 };
 
