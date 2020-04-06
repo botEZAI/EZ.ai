d2c61d409c9c37826b4815fa5a8313608c4f69d5
junyeongjang <junyeong.dev@gmail.com>
2020-04-06 15:16:56 +0900

    Update server.js
    flash 메세지 추가

--- ez.ai/server.js
+++ ez.ai/server.js
@@ -3,42 +3,37 @@ const bodyParser = require("body-parser");
 const passport = require('passport'); // passport 패키지 설치
 const port = process.env.PORT || 5000; //5000포트 사용 react는 3000포트 proxy로 연결중
 const flash = require('connect-flash'); //일회용 메세지를 출력하는 미들웨어
+const session = require('express-session'); //세선관리용 미들웨어
 
 const cookieParser = require("cookie-parser");
 const sequelize = require("./models").sequelize;
-//const passportConfig = require('./passport'); // passport 모듈 연결
+const passportConfig = require('./passport'); // passport 모듈 연결
 
 const app = express();
 sequelize.sync();
 passportConfig(passport); //passport
 
 //라우터 연결
-const chatbotRouter = require("./routes/chatbot"); // 키워드 + 콘텐트 라우터
-const imageRouter = require("./routes/image"); //이미지 라우터
-const videoRouter = require("./routes/video"); //비디오 라우터
-const audioRouter = require("./routes/audio"); //오디오 라우터
-const fileRouter = require("./routes/file"); //파일 라우터
-
-//수정,삭제 라우터 추가 
-// var fileChatbotUpdata = require("./routes/chatbotUpdate")// 챗봇 내용 수정 라우터
-// var fileRouterUpdate = require("./routes/fileUpdate"); //파일 수정 라우터
-// var imageRouterUpdate = require("./routes/imageUpdate"); //이미지 라우터
-// var videoRouterUpdate = require("./routes/videoUpdate"); //비디오 라우터
-// var audioRouterUpdate = require("./routes/audioUpdate"); //오디오 라우터
-// var fileRouterUpdate = require("./routes/fileUpdate"); //파일 라우터
-
-//삭제 라우터 추가 
-// var fileChatbotDel = require("./routes/chatbotDel")// 챗봇 내용 삭제 라우터
-//var fileRouterDel = require("./routes/fileDel"); //파일 삭제 라우터
-// var imageRouterDel = require("./routes/imageDel"); //이미지 삭제 라우터
-// var videoRouterDel = require("./routes/videoDel"); //비디오 삭제 라우터
-// var audioRouterDel = require("./routes/audioDel"); //오디오 삭제 라우터
-// var fileRouterDel = require("./routes/fileDel"); //파일 삭제 라우터
-
+const chatbotRouter = require('./routes/chatbot'); // 키워드 + 콘텐트 라우터
+const imageRouter = require('./routes/image'); //이미지 라우터
+const videoRouter = require('./routes/video'); //비디오 라우터
+const audioRouter = require('./routes/audio'); //오디오 라우터
+const fileRouter = require('./routes/file'); //파일 라우터
+const authRouter = require('./routes/auth'); //로그인 라우터
+// 세션관리 + cookiparser 미들웨어
+app.use(cookieParser('secret code'));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
 
 //미들웨어 사용
 app.use("/", express.static("objects"));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());// flash 미들웨어
@@ -52,6 +47,7 @@ app.use("/api/image", imageRouter); //이미지
 app.use("/api/video", videoRouter); //비디오
 app.use("/api/audio", audioRouter); //오디오
 app.use("/api/file", fileRouter); //파일
+app.use('/api/user',authRouter); //로그인
 
 // 아직 에러 처리부분 없음
 app.listen(port, () => console.log(`Listening on port ${port}`));
