a0030fb589e0e56fac3634de8086893970ffd9d4
CHOIKEONWOO <easter9211@gmail.com>
2020-04-07 01:06:01 +0900

    가입,로그인 후 페이지 이동

--- ez.ai/routes/auth.js
+++ ez.ai/routes/auth.js
@@ -1,63 +1,66 @@
 //회원가입, 로그인, 로그아웃 라우터
 
-const express = require('express');
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-const { isLoggedIn, isNotLoggedIn }  = require('./middlewares');
-const { User } = require('../models');
+const express = require("express");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const { User } = require("../models");
 
 const router = express.Router();
 
-router.post('/', isNotLoggedIn, async(req, res, next) => { //회원가입
-    const { email, password, userName, nickName, birthday } = req.body;
-    try{
-        const exUser = await User.findOne({ where : { email }});
-        if (exUser){
-            req.flash('joinError', '이미 가입된 메일입니다');
-            console.log("이미 가입된 메일입니다.");
-            return res.json('회원 가입 실패');
-        }
+router.post("/", isNotLoggedIn, async (req, res, next) => {
+  //회원가입
+  const { email, password, userName, nickName, birthday } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      req.flash("joinError", "이미 가입된 메일입니다");
+      console.log("이미 가입된 메일입니다.");
+      return res.status(403).send("이미 사용중인 이메일입니다.");
+    }
     const hash = await bcrypt.hash(password, 12);
     console.log(hash);
     await User.create({
-        email: email,
-        password: hash,
-        nick: nickName,
-        name: userName,
-        birth: birthday, 
+      email: email,
+      password: hash,
+      nick: nickName,
+      name: userName,
+      birth: birthday,
     });
-        return res.json('회원 가입 성공');
-    } catch (error){
-        console.error(error);
-        return next(error);
-    }   
+    return res.json("회원 가입 성공");
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
-router.post('/login', isNotLoggedIn, (req, res,next) => { //로그인
-    console.log(req.body);
-    passport.authenticate('local', (authError, user, info)=>{
-        if (authError){
-            console.error(authError);
-            return next(authError);
-        }
-        if (!user){
-            req.flash('loginError', info.message);
-            return res.redirect('/');    
-        }
-        return req.login(user, (loginError) =>{
-            if (loginError){
-            console.error(loginError);
-            return next(loginError);
-        }
-        return res.redirect('/');
-        });
-    })(req, res, next);
+router.post("/login", isNotLoggedIn, (req, res, next) => {
+  //로그인
+  console.log(req.body);
+  passport.authenticate("local", (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      req.flash("loginError", info.message);
+      return res.redirect("/");
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
 });
 
-router.get('/logout', isLoggedIn, (req,res) =>{ //로그아웃
-    req.logout();
-    req.session.destory();
-    res.redirect('/');
+router.get("/logout", isLoggedIn, (req, res) => {
+  //로그아웃
+  req.logout();
+  req.session.destory();
+  res.redirect("/");
 });
 
 module.exports = router;
--- ez.ai/server.js
+++ ez.ai/server.js
@@ -1,53 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const passport = require('passport'); // passport 패키지 설치
+const passport = require("passport"); // passport 패키지 설치
 const port = process.env.PORT || 5000; //5000포트 사용 react는 3000포트 proxy로 연결중
-const flash = require('connect-flash'); //일회용 메세지를 출력하는 미들웨어
-const session = require('express-session'); //세선관리용 미들웨어
+const flash = require("connect-flash"); //일회용 메세지를 출력하는 미들웨어
+const session = require("express-session"); //세선관리용 미들웨어
 
 const cookieParser = require("cookie-parser");
 const sequelize = require("./models").sequelize;
-const passportConfig = require('./passport'); // passport 모듈 연결
+const passportConfig = require("./passport"); // passport 모듈 연결
 
 const app = express();
 sequelize.sync();
 passportConfig(passport); //passport
 
 //라우터 연결
-const chatbotRouter = require('./routes/chatbot'); // 키워드 + 콘텐트 라우터
-const imageRouter = require('./routes/image'); //이미지 라우터
-const videoRouter = require('./routes/video'); //비디오 라우터
-const audioRouter = require('./routes/audio'); //오디오 라우터
-const fileRouter = require('./routes/file'); //파일 라우터
-const authRouter = require('./routes/auth'); //로그인 라우터
+const chatbotRouter = require("./routes/chatbot"); // 키워드 + 콘텐트 라우터
+const imageRouter = require("./routes/image"); //이미지 라우터
+const videoRouter = require("./routes/video"); //비디오 라우터
+const audioRouter = require("./routes/audio"); //오디오 라우터
+const fileRouter = require("./routes/file"); //파일 라우터
+const authRouter = require("./routes/auth"); //로그인 라우터
 // 세션관리 + cookiparser 미들웨어
-app.use(cookieParser('secret code'));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: 'secret code',
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
+app.use(cookieParser("secret code"));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "secret code",
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "Ez.ai",
+  })
+);
 
 //미들웨어 사용
 app.use("/", express.static("objects"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(flash());// flash 미들웨어
+app.use(flash()); // flash 미들웨어
 app.use(passport.initialize()); //passport 사용
-app.use(passport.session()); //passport 사용 
+app.use(passport.session()); //passport 사용
 
- 
 //라우터 사용
 app.use("/api/chatbotbuild", chatbotRouter); // 키워드 + 콘텐트
 app.use("/api/image", imageRouter); //이미지
 app.use("/api/video", videoRouter); //비디오
 app.use("/api/audio", audioRouter); //오디오
 app.use("/api/file", fileRouter); //파일
-app.use('/api/user',authRouter); //로그인
+app.use("/api/user", authRouter); //로그인
 
 // 아직 에러 처리부분 없음
 app.listen(port, () => console.log(`Listening on port ${port}`));
