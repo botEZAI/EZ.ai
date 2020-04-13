ba4c8132e01fc6c6dd9c8ade0af2d8a273f7dd75
ChayanRin <k852012@naver.com>
2020-04-13 15:34:22 +0900

    Update NavPopup basic interfaces

--- ez.ai/client/src/NavPopup.js
+++ ez.ai/client/src/NavPopup.js
@@ -1,13 +1,16 @@
 import React from "react"
+import {Link} from "react-router-dom";
 
 const NavPopup = (
     onLogout
 ) => {
     return (
         <div className="NavPopUp">
-            <button className="logout" onClick={onLogout}>
-                로그아웃
-            </button>
+            <div className="NavPopUp-btns">
+                <div className = "NavPopUp-btn myInfo" ><Link to="/profile">내정보</Link></div>
+                <div className= "NavPopUp-btn logout" onClick={onLogout}>로그아웃</div>
+            </div>
+
         </div>
     )
 };
