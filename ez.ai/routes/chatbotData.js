b0391e3085370d47bb9328e1bf23b84fe389b017
junyeongjang <junyeong.dev@gmail.com>
2020-04-14 21:23:21 +0900

    Update chatbotData.js
    오류 수정

--- ez.ai/routes/chatbotData.js
+++ ez.ai/routes/chatbotData.js
@@ -9,10 +9,7 @@ router.get('/', async (req, res, next) => {
   try {
     console.log("데이터는", req);
     const chatbotData = await ChatbotData.findAll({
-      include:{
-        model: User,
-        where: {user_id: User.id}, //join 되어있는 chatbot 목록을 가져옴
-        },
+        where: {user_id: req.user.id}, //join 되어있는 chatbot 목록을 가져옴
     });
     res.json(chatbotData); //프론트에 데이터 전송
   } catch (e) {
@@ -23,16 +20,16 @@ router.get('/', async (req, res, next) => {
 
 //챗봇 등록
 router.post('/', isLoggedIn, async (req, res, next) => {
+  //console.log(req);
   try {
     const newChatbot = await ChatbotData.create({
-
       username: req.body.user, // 사용자 이름
       botname: req.body.botname, //봇 이름
       sns: req.body.sns,   // 어떤 플랫폼인지
       desc: req.body.desc,  // 챗봇 설명
       token: req.body.token, //토큰 삽입
       data: JSON.stringify(req.body.data), //나중에 삭제 해야함
-      user_id: req.body.id, //유저 ID 외래 키 
+      user_id: req.user.id, //유저 ID 외래 키 
       categories: JSON.stringify(req.body.categories),
     });
     res.json(newChatbot);
