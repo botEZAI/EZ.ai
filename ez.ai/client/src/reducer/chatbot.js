bdf25c5c56da60e6d4c467e9cf05cd3d3508ef8a
CHOIKEONWOO <easter9211@gmail.com>
2020-04-15 21:17:01 +0900

    Update, Delete Chatbot

--- ez.ai/client/src/reducer/chatbot.js
+++ ez.ai/client/src/reducer/chatbot.js
@@ -2,6 +2,8 @@ export const initialState = {
   chatbotList: [], //챗봇리스트
   currentChatbot: null, //현재 클릭된 챗봇
   currentCategories: null, //현재 클릭된 챗봇의 카테고리 리스트
+  isUpdateSuccess: false, //업데이트 성공여부
+  isDeleteSuccess: false, //삭제 성공여부
 };
 
 export const ADD_CHATBOT_REQUEST = "ADD_CHATBOT_REQUEST";
@@ -11,6 +13,15 @@ export const ADD_CHATBOT_FAILURE = "ADD_CHATBOT_FAILURE";
 export const LOAD_CHATBOT_REQUEST = "LOAD_CHATBOT_REQUEST";
 export const LOAD_CHATBOT_SUCCESS = "LOAD_CHATBOT_SUCCESS";
 export const LOAD_CHATBOT_FAILURE = "LOAD_CHATBOT_FAILURE";
+
+export const UPDATE_CHATBOT_REQUEST = "UPDATE_CHATBOT_REQUEST";
+export const UPDATE_CHATBOT_SUCCESS = "UPDATE_CHATBOT_SUCCESS";
+export const UPDATE_CHATBOT_FAILURE = "UPDATE_CHATBOT_FAILURE";
+export const UPDATE_CHATBOT_SUCCESS_RESET = "UPDATE_CHATBOT_SUCCESS_RESET";
+
+export const DELETE_CHATBOT_REQUEST = "DELETE_CHATBOT_REQUEST";
+export const DELETE_CHATBOT_SUCCESS = "DELETE_CHATBOT_SUCCESS";
+export const DELETE_CHATBOT_FAILURE = "DELETE_CHATBOT_FAILURE";
 
 export const SET_CURRENT_CHATBOT = "SET_CURRENT_CHATBOT";
 
@@ -25,7 +36,6 @@ export default (state = initialState, action) => {
       return {
         ...state,
         chatbotList: [...state.chatbotList, action.data],
-        currentCategories: action.data.categories,
       };
     }
     case ADD_CHATBOT_FAILURE: {
@@ -49,13 +59,54 @@ export default (state = initialState, action) => {
         ...state,
       };
     }
+    case UPDATE_CHATBOT_REQUEST: {
+      return {
+        ...state,
+      };
+    }
+    case UPDATE_CHATBOT_SUCCESS: {
+      return {
+        ...state,
+        chatbotList: action.data,
+        isUpdateSuccess: true,
+      };
+    }
+    case UPDATE_CHATBOT_FAILURE: {
+      return {
+        ...state,
+      };
+    }
+    case UPDATE_CHATBOT_SUCCESS_RESET: {
+      return {
+        ...state,
+        isUpdateSuccess: false,
+      };
+    }
+    case DELETE_CHATBOT_REQUEST: {
+      return {
+        ...state,
+      };
+    }
+    case DELETE_CHATBOT_SUCCESS: {
+      return {
+        ...state,
+        chatbotList: action.data,
+        isDeleteSuccess: true,
+      };
+    }
+    case DELETE_CHATBOT_FAILURE: {
+      return {
+        ...state,
+      };
+    }
     case SET_CURRENT_CHATBOT: {
       return {
         ...state,
         currentChatbot: action.data,
-        currentCategories: action.data.categories,
+        currentCategories: JSON.parse(action.data.categories),
       };
     }
+
     default: {
       return {
         ...state,
--- ez.ai/routes/chatbotData.js
+++ ez.ai/routes/chatbotData.js
@@ -1,15 +1,14 @@
 var express = require("express");
 var router = express.Router();
-const ChatbotData = require('../models').ChatbotData;
-const User = require('../models').User;
+const ChatbotData = require("../models").ChatbotData;
+const User = require("../models").User;
 const { isLoggedIn } = require("./middlewares");
 
 //챗봇 로딩
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    console.log("데이터는", req);
     const chatbotData = await ChatbotData.findAll({
-        where: {user_id: req.user.id}, //join 되어있는 chatbot 목록을 가져옴
+      where: { user_id: req.user.id }, //join 되어있는 chatbot 목록을 가져옴
     });
     res.json(chatbotData); //프론트에 데이터 전송
   } catch (e) {
@@ -19,20 +18,53 @@ router.get('/', async (req, res, next) => {
 });
 
 //챗봇 등록
-router.post('/', isLoggedIn, async (req, res, next) => {
-  // console.log(req);
+router.post("/", isLoggedIn, async (req, res, next) => {
   try {
     const newChatbot = await ChatbotData.create({
       username: req.body.user, // 사용자 이름
       botname: req.body.botname, //봇 이름
-      sns: req.body.sns,   // 어떤 플랫폼인지
-      desc: req.body.desc,  // 챗봇 설명
+      sns: req.body.sns, // 어떤 플랫폼인지
+      desc: req.body.desc, // 챗봇 설명
       token: req.body.token, //토큰 삽입
       data: JSON.stringify(req.body.data), //나중에 삭제 해야함
       user_id: req.user.id, //유저 ID 외래 키, req.user.id 는 passport에 localStrategy에서 옴
       categories: JSON.stringify(req.body.categories),
     });
     res.json(newChatbot);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+//챗봇 업데이트
+router.patch("/", isLoggedIn, async (req, res, next) => {
+  try {
+    await ChatbotData.update(
+      {
+        data: JSON.stringify(req.body.data),
+        categories: JSON.stringify(req.body.categories),
+      },
+      {
+        where: { id: req.body.id },
+      }
+    );
+    const chatbotData = await ChatbotData.findAll({
+      where: { user_id: req.user.id },
+    });
+    res.json(chatbotData);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+//챗봇 삭제
+router.delete("/", isLoggedIn, async (req, res, next) => {
+  try {
+    await ChatbotData.destroy({ where: { id: req.body.id } });
+    const chatbotData = await ChatbotData.findAll({
+      where: { user_id: req.user.id },
+    });
+    res.json(chatbotData);
   } catch (e) {
     console.error(e);
     next(e);
