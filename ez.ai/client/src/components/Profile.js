89aeeeed50420fab8d282006b7c6f62c1d069f42
ChayanRin <k852012@naver.com>
2020-04-15 15:08:31 +0900

    Update profile basic interfaces

--- ez.ai/client/src/components/Profile.js
+++ ez.ai/client/src/components/Profile.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Profile.css";
+import {useDispatch, useSelector} from "react-redux";
+import {LOAD_USER_REQUEST} from "../reducer/user";
 
 const Profile = () => {
+    const { user } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!user) {
+            dispatch({
+                type: LOAD_USER_REQUEST,
+            });
+        }
+    }, [user]);
+
+
   return (
       <div className="user-info">
         <div className = "user-info__title">
@@ -14,22 +28,43 @@ const Profile = () => {
               <div className="u-profile__more">
                   <div className= "u-more__column">
                       <div className="u-more__picture">
-                            사진
+                          <i className="far fa-images"></i>
                       </div>
                   </div>
                   <div className= "u-more__column">
                       <div className = "u-more__name">
-                          별명 ( 수정가능하게)
+                          별명 : XXX
                       </div>
+                      <div className = "u-more__name-btn">수정</div>
                   </div>
                   
               </div>
           </div>
           <div className="user-info__more">
-              개인정보 - 이름, 생일, 이메일,비밀번호 (변경기능까지)
+              <div className="u-title">
+                  개인정보
+              </div>
+              <div className="u-profile__more">
+                  <div className="u-more__infos">
+                      <div className="u-more__info">이름 : </div>
+                      <div className="u-more__info">생일 : </div>
+                      <div className="u-more__info">이메일 : </div>
+                      <div className="u-more__info">비밀번호 : </div>
+                      <div className = "u-more__name-btn">정보수정</div>
+                  </div>
+
+              </div>
           </div>
           <div className="user-info__chatbot">
-              생성한 챗복 목록 단순하게 보여줌, 챗봇 생성, 수정하러가기(버튼 -> 챗봇목록으로 넘어감)
+              <div className="u-title">
+                  생성한 챗봇 목록
+              </div>
+              <div className="u-chatbots">
+                  챗봇목록들 간단하게 보여줌
+              </div>
+              <div className="u-more__name-btn u-chatbots__detail">
+                  자세히보기
+              </div>
           </div>
       </div>
 )
