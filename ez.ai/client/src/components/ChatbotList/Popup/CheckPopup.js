4995bc704eed8a2d44bf474e663976573765ad99
ChayanRin <k852012@naver.com>
2020-05-19 19:31:42 +0900

    Update chatbot create popup

--- ez.ai/client/src/components/ChatbotList/Popup/CheckPopup.js
+++ ez.ai/client/src/components/ChatbotList/Popup/CheckPopup.js
@@ -5,7 +5,9 @@ const CheckPopup = ({
   isOpen,
   close,
   next,
-  finish
+  finish,
+  botName,
+  botDesc,
 }) => {
   const undoToTokenInput = () => {
     next("first");
@@ -16,10 +18,22 @@ const CheckPopup = ({
         <React.Fragment>
           <div className="popup-overlay" onClick={close}></div>
           <div className="popup">
-            챗봇을 만드시겠습니까?
+            <div className ="popup-check-title">
+              챗봇을 만드시겠습니까?
+            </div>
+            <div className="popup-check-values">
+              <div className="popup-check-value">
+                <div className="popup-check-name">
+                  봇 이름 : <span>{botName.name}</span>
+                </div>
+                <div className="popup-check-desc">
+                  설명 : <span>{botDesc.desc}</span>
+                </div>
+              </div>
+            </div>
             <div className="popup-button-wrap">
               <button onClick={undoToTokenInput}>이전</button>
-              <button onClick={finish}>완료</button>
+              <button onClick={finish}>생성</button>
             </div>
           </div>
         </React.Fragment>
