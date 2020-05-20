11e1c941b846973ab9a299c78a21cb72014738dc
ChayanRin <k852012@naver.com>
2020-05-20 19:43:55 +0900

    Update header and nav
    헤더와 네비게이션 인터페이스 업데이트

--- ez.ai/client/src/components/Header.js
+++ ez.ai/client/src/components/Header.js
@@ -18,52 +18,75 @@ const Header = ({ history }) => {
   }, [user]);
 
   const [showNP, setShowNP] = useState(false);
+
+  const createChatbot = () => {
+    if (!user) {
+      alert("먼저 로그인해주시기 바랍니다.");
+    }
+  }
+
   const clickNavPopup = () => {
     setShowNP(!showNP);
   };
 
   return (
     <div className="header">
-      <div className="logo_default">
-        <div>
-          <Link to="/">Ez.ai</Link>
+      <div className="nav_l">
+        <div className="logo_default">
+          <div>
+            <Link to="/">Ez.ai</Link>
+          </div>
+        </div>
+        <div className="nav">
+          <ul>
+            <li>
+              <Link to="/about">소개</Link>
+            </li>
+            <li>
+              <Link to="/guide">가이드라인</Link>
+            </li>
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className="nav">
-        <ul>
-          <li>
-            <Link to="/about">소개</Link>
-          </li>
-          <li>
-            <Link to="/guide">가이드라인</Link>
-          </li>
-          <li>
-            <Link to="/faq">FAQ</Link>
-          </li>
-        </ul>
-      </div>
-
+      <div className="nav_r">
       {!user ? (
-        <div className="nav_right login">
-          <Link to="/login">로그인</Link>
+        <div className="nav_right">
+          <div className = "nav_login">
+            <Link to="/login">로그인</Link>
+          </div>
+          <div className = "nav_register">
+            <Link to="/register">회원가입</Link>
+          </div>
         </div>
       ) : (
           <>
-          <div className= "">
-            <Link to="/chatbotlist">
-              <div>챗봇목록</div>
-            </Link>
           <div className="nav_right profile" onClick={clickNavPopup}>
 
             <div className="profile-picture">
               <i className="fas fa-user-circle"></i>
             </div>
-            <div className="profile-more">{user.name}님</div>
-          </div>
+            <div className="profile-more">{user.name} 님&nbsp;{!showNP ? <i className="fas fa-chevron-down"></i> :
+                <i className="fas fa-chevron-up"></i>}</div>
           </div>
           </>
       )}
+        <div className="nav_chatbot">
+          <div className="nav_chatbot-btn" onClick={createChatbot}>
+            {!user ? (
+                <Link to = "/login">챗봇만들기</Link>
+            ) : (
+                <Link to = "/chatbotlist">챗봇만들기</Link>
+            )}
+          </div>
+        </div>
+      </div>
+
       {showNP ? <NavPopup showNP={showNP} setShowNP={setShowNP} /> : null}
+
+
     </div>
   );
 };
