1afb80a4190dc17dcae43756ec19b85a64962289
ChayanRin <k852012@naver.com>
2020-06-01 02:13:37 +0900

    Create presist menu status basic (uncomplete)
    고정메뉴 속성 수정 영역 생성(미완)
      - 수정의 키워드명 적는 칸에 적으면 만들어야되는 배열의 text 속성에 수정되게끔
      - 이 속성 읽어서 본 요소의 영역에서 수정한 내용 적용되게
      - 기존 키워드 영역에서 onChange 안먹힘 (useEffect같은거 사용)

--- ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/PersistentStatus.js
+++ ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/PersistentStatus.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import produce from "immer";
-import KeywordPopUp from "../StatusPopups/KeywordPopUp";
 
 const PersistentStatus = ({
   currentContent,
@@ -32,8 +31,30 @@ const PersistentStatus = ({
     );
   };
 
+  const [selectedBtn, setSelectedBtn] = useState(0);
+  const [btnText,setBtnText] = useState("");
+
+  const setKeyword = (i) => {
+    setBtnText(i)
+  }
+
+  const onChangeBtnText = e => {
+    setBtnText(e.target.value);
+    
+  }
+
+  const setListKeyword = keyword => {
+    setKeywordObject(
+        produce(keywordObject, draft => {
+          draft[index].contents[now].listContent.keywordLink[
+              selectedBtn
+              ] = keyword;
+        })
+    );
+  };
+
   return (
-    <>
+    <div className="status-list-main">
       <div className="status-input status-list">
         <textarea
           placeholder="작성하고자 하는 텍스트를 적어주세요"
@@ -57,45 +78,47 @@ const PersistentStatus = ({
         </div>
         <table>
           {curListCount.map((i) => (
-            <div className="status-list-content">
+            <div className="status-list-content" onClick = {() => {setSelectedBtn(i)}}>
               <input
-                placeholder="키워드명을 적어주세요"
-                value={
-                  keywordObject[index].contents[now].listContent.elem[i] || ""
-                }
-                onChange={(e) => {
-                  setKeywordObject(
-                    produce(keywordObject, (draft) => {
-                      draft[index].contents[now].listContent.elem[i] =
-                        e.target.value;
-                    })
-                  );
-                }}
+                placeholder="연동된 키워드가 없습니다."
+                readOnly={true}
+                value={btnText}
+
               />
-              <div
-                className="list-keyword-btn"
-                name={i}
-                onClick={() => toggleKeywordPopUp(i)}
-              >
-                {keywordObject[index].contents[now].listContent.keywordLink[
-                  i
-                ] || "연동"}
-                {showPopup && popNum === i ? (
-                  <KeywordPopUp
-                    keywordObject={keywordObject}
-                    setKeywordObject={setKeywordObject}
-                    currentContent={currentContent}
-                    currentElemIndex={i}
-                    now={now}
-                    index={index}
-                  />
-                ) : null}
-              </div>
             </div>
           ))}
         </table>
       </div>
-    </>
+      <div className="status-list-modify">
+        <div className="status-list-modify-title">키보드 수정</div>
+        <div className="status-list-modify-contents">
+          {selectedBtn === 0 ? (
+              <div className="status-list-modify-none">선택된 고정메뉴 버튼이 없습니다..</div>
+          ) : (
+              <>
+                <div className="status-list-modify-content-num">{selectedBtn}번째 버튼 : </div>
+                <input placeholder="키워드명을 적어주세요" value={btnText} onChange = {onChangeBtnText} />
+              <select onChange = {(e) => {setKeyword(e.target.value)}}>
+              <option value="none" disabled>=== 키워드 선택 ===</option>
+              {keywordObject.map((keyword, index) => {
+                return (
+                    <>
+                      <option
+                          key={index}
+                          value={keyword.keyword}
+                          onClick={() => setListKeyword(keyword.keyword)}
+                      >
+                        {keyword.keyword}
+                      </option>
+                    </>
+                )
+              })}
+              </select>
+                </>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
