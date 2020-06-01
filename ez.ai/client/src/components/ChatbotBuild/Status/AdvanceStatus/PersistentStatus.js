b69093adadf7a03a17a21bb4437832df7abaf056
ChayanRin <k852012@naver.com>
2020-06-01 13:54:10 +0900

    Create presistmenu status modify section
    현재 고정메뉴 키보드 요소 수정 완료.
    참고사항은 키워드 목록에서 선택하는것 뿐만 아니라
    텍스트도 넣을수 있게해서
    이후에 include 봇 api 이용해서 해당 키워드 들어가 있으면 실행되게끔 할수 있게 만들어둠

--- ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/PersistentStatus.js
+++ ez.ai/client/src/components/ChatbotBuild/Status/AdvanceStatus/PersistentStatus.js
@@ -13,14 +13,6 @@ const PersistentStatus = ({
   curListCount,
   setCurListCount,
 }) => {
-  const [showPopup, setShowPopup] = useState(false);
-  const [popNum, setPopNum] = useState("-1");
-
-
-  const toggleKeywordPopUp = (i, e) => {
-    setPopNum(i);
-    setShowPopup(!showPopup);
-  };
 
   const changeListLength = (i) => {
     setCurListCount(listCount[i - 1]);
@@ -33,25 +25,40 @@ const PersistentStatus = ({
 
   const [selectedBtn, setSelectedBtn] = useState(0);
   const [btnText,setBtnText] = useState("");
+  const [keywords, setKeywords] = useState([
+      {id : 0, text:""},
+      {id : 1, text:""},
+      {id : 2, text:""},
+      {id : 3, text:""},
+      {id : 4, text:""},
+      {id : 5, text:""},
+      {id : 6, text:""},
+  ]);
 
   const setKeyword = (i) => {
     setBtnText(i)
   }
 
-  const onChangeBtnText = e => {
-    setBtnText(e.target.value);
-    
+  const chooseKeywordBtn = i => {
+    setSelectedBtn(i)
+    setBtnText(keywords[i].text)
   }
 
-  const setListKeyword = keyword => {
+  const onChangeBtnText = e => {
+    setKeywords(keywords.map(keyword =>
+        keyword.id === selectedBtn ? {...keyword, text : e.target.value} : keyword
+    ))
     setKeywordObject(
         produce(keywordObject, draft => {
-          draft[index].contents[now].listContent.keywordLink[
-              selectedBtn
-              ] = keyword;
-        })
-    );
-  };
+          draft[index].contents[now].listContent.keywordLink[selectedBtn] = e.target.value;
+        }))
+    setBtnText(e.target.value);
+
+
+    console.log(keywords, selectedBtn, btnText, keywordObject[index].contents[now].listContent)
+  }
+
+
 
   return (
     <div className="status-list-main">
@@ -59,14 +66,6 @@ const PersistentStatus = ({
         <textarea
           placeholder="작성하고자 하는 텍스트를 적어주세요"
           value={keywordObject[index].contents[now].listContent.question || ""}
-          onChange={(e) => {
-            setKeywordObject(
-              produce(keywordObject, (draft) => {
-                draft[index].contents[now].listContent.question =
-                  e.target.value;
-              })
-            );
-          }}
         ></textarea>
         <div className="list-count">
           <span>선택지 개수 : </span>
@@ -78,12 +77,11 @@ const PersistentStatus = ({
         </div>
         <table>
           {curListCount.map((i) => (
-            <div className="status-list-content" onClick = {() => {setSelectedBtn(i)}}>
+            <div className={i === selectedBtn ? "status-list-content current-list-content" : "status-list-content"} onClick = {() => {chooseKeywordBtn(i)}}>
               <input
                 placeholder="연동된 키워드가 없습니다."
                 readOnly={true}
-                value={btnText}
-
+                value={keywords[i].text}
               />
             </div>
           ))}
@@ -96,17 +94,19 @@ const PersistentStatus = ({
               <div className="status-list-modify-none">선택된 고정메뉴 버튼이 없습니다..</div>
           ) : (
               <>
-                <div className="status-list-modify-content-num">{selectedBtn}번째 버튼 : </div>
-                <input placeholder="키워드명을 적어주세요" value={btnText} onChange = {onChangeBtnText} />
-              <select onChange = {(e) => {setKeyword(e.target.value)}}>
-              <option value="none" disabled>=== 키워드 선택 ===</option>
+                <div className="status-list-modify-content-input">
+                  <span>{selectedBtn}번째 버튼 : </span>
+                  <input placeholder="키워드명을 적어주세요" value={btnText} onChange = {onChangeBtnText} />
+                </div>
+
+              <select onChange = {onChangeBtnText}>
+              <option value="none" selected disabled>=== 키워드 선택 ===</option>
               {keywordObject.map((keyword, index) => {
                 return (
                     <>
                       <option
                           key={index}
                           value={keyword.keyword}
-                          onClick={() => setListKeyword(keyword.keyword)}
                       >
                         {keyword.keyword}
                       </option>
