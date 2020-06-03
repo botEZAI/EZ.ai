97cff932e342f93d5686969b47001d2584ade9d2
ChayanRin <k852012@naver.com>
2020-06-03 17:27:25 +0900

    Update basic GuideLine layout

---
+++ ez.ai/client/src/components/Guide/step2/telegramChatbotConnect.js
@@ -0,0 +1,11 @@
+import react from "React";
+
+const telegramChatbotConnect = () => {
+    const contents = `
+    # 텔레그램 챗봇 연동
+    
+    텔레그램 챗봇 연동 방법입니다!
+    `;
+}
+
+export default telegramChatbotConnect;
