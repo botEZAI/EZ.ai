6a001173f6e320aeb3c6562765889d9f1faa6502
junyeongjang <junyeong.dev@gmail.com>
2020-06-10 02:52:59 +0900

    Update video.js
    1. 비디오 s3 작업 추가

--- ez.ai/routes/video.js
+++ ez.ai/routes/video.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const multerS3 = require('multer-s3');
+const fs = require('fs'); const path = require('path'); 
+const AWS = require('aws-sdk');
 const router = express.Router();
 
 // multer 처리부분
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
+  region : 'ap-northeast-2'
+});
+
+const s3 = new AWS.S3();
 const upload = multer({
-    storage: multer.diskStorage({
-        destination(req,file, cb){ //경로 설정 
-            cb(null, 'objects/');
-        },
-        filename(req, file, cb){ // 파일 이름 설정
-            const ext = path.extname(file.originalname);
-            cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext);
-        },
-    }),
-    limits: {fileSize: 200 * 1024 * 1024}, //limits 용량 논의 필요 200MB
+  storage: multerS3({
+      s3: s3,
+      bucket: "ezai", // 버킷 이름
+      contentType: multerS3.AUTO_CONTENT_TYPE, // 자동을 콘텐츠 타입 세팅
+      acl: 'public-read', // 클라이언트에서 자유롭게 가용하기 위함
+      key: (req, file, cb) => {
+          console.log(file);
+          cb(null, file.originalname)
+      },
+  }),
+  limits: { fileSize: 200 * 1024 * 1024 }, // 용량 제한
 });
 
 
 router.post('/', upload.single('video'), (req, res) =>{  // 'video'가 일치해야함 리액트랑 
-
     //리액트에 주소 보내는 방식은 이야기 해야함 !
     console.log("파일 전송 성공");
     console.log(req.file);
