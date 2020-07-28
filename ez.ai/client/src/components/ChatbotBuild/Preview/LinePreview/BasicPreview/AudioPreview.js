8c31b34327883d79c475560702909d6b2b39d024
giraff <hse05105@naver.com>
2020-07-29 01:17:36 +0900

    Update Audio element
    - 인터페이스 최대한 비슷하게
    - 아이콘 누르면 재생 및 일시 정지
    - 텔레그램의 경우 오디오 정보로 데스크톱은 시간, 모바일은 파일 크기(KB)가 뜬다. 통일감을 위해 시간으로 정함.
    - 라인의 경우 오디오 파형 애니메이션이 옆에 들어가는 것 보류. 일단 시간 정보만 뜬다.

--- ez.ai/client/src/components/ChatbotBuild/Preview/LinePreview/BasicPreview/AudioPreview.js
+++ ez.ai/client/src/components/ChatbotBuild/Preview/LinePreview/BasicPreview/AudioPreview.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useRef } from "react"
 
 const AudioPreview = ({
   v,
@@ -9,11 +9,53 @@ const AudioPreview = ({
   onDelete,
   changeAvailableIcon
 }) => {
+  const audio = useRef(null);
+  const audioTime = useRef(null);
+  const [playState, setPlayState] = useState(false);
+
+  const clickHandler = () => {
+    setPlayState(!playState);
+    if(audio.current !== null) {
+      if(!playState){
+        audio.current.play();
+      }
+      else {
+        audio.current.pause();
+      }
+    }
+  }
+
+  const funcDuration = (duration) => {
+    const durMin = (duration * .001) < 60 ? 0 : Math.floor((duration *.001) / 60);
+    const durSec = Math.floor(duration * .001 - (durMin * 60));
+
+    return `${durMin}:${durSec < 10 ? `0${durSec}` : durSec}`;
+  }
+
+  const func = (e) => {
+  
+    const time = e.target.currentTime; // 현재 카운트 되는 시간
+    const totalTime = e.target.duration; // 비디오의 전체 재생 시간
+    const minutes = time < 60 ? 0 : Math.floor(time / 60); 
+    const seconds = Math.floor(time - (minutes * 60));
+    const totalMin = totalTime < 60 ? 0 : Math.floor(totalTime / 60);  
+    const totalSec = Math.floor(totalTime - (totalMin * 60));
+    const showMin = totalMin - minutes;
+    const showSec = totalSec - seconds; 
+
+    audioTime.current.innerHTML = `${showMin}:${showSec < 10 ? `0${showSec}` : showSec}`;
+    if(playState && e.target.ended) {
+      setPlayState(false);
+      audioTime.current.innerHTML = `${totalMin}:${totalSec < 10 ? `0${totalSec}` : totalSec}`
+    }
+  }
+  
+
   return (
       <div className="main-preview">
       <div
-        className={now === i ? "main-content audiobox-telegram now"
-                            : "main-content audiobox-telegram"}
+        className={now === i ? "main-content audiobox-line now"
+                            : "main-content audiobox-line"}
         key={v.content + i}
         onClick={() => {
           setClickedMainInput(v);
@@ -21,17 +63,27 @@ const AudioPreview = ({
           changeAvailableIcon("audio");
         }}
       >
-        {" "}
-        <div>
-          <i className="fas fa-play fa-lg file-icon-telegram"></i>
-          <div className="file-content-telegram">
-            <div className="file-name" data-filetype="">
-              {v.content}
-            </div>
-            <div className="file-size" data-filetype="">
-              {/* 길이, 용량을 표시하려면 데이터를 확장해야함.현재는 파일 이름만 저장 */}
-              {/* 00:00, 00.00 MB{" "} */}
-            </div>
+        {playState && v.content !== "" 
+        ? 
+          <i className="fas fa-pause-circle fa-lg audio-icon-line" 
+              onClick={clickHandler}></i>
+        :
+          <i className="fas fa-play-circle fa-lg audio-icon-line" 
+              onClick={clickHandler}></i>
+        }
+
+        <div className="audio-contents-line">
+          <audio 
+            ref={audio} 
+            src={v.content} 
+            onTimeUpdate={(e) => func(e)} 
+          />
+          <div className="audio-name" ref={audioTime}>
+
+            {v.content !== "" && v.size !== null ? 
+              funcDuration(v.size)
+            :
+              "0:00"}
           </div>
         </div>
       </div>
