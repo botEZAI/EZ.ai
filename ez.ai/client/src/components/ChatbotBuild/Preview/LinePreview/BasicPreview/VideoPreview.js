41a4b51c36141c18115039b3a955f27a1e179ab6
giraff <hse05105@naver.com>
2020-08-01 17:04:37 +0900

    Update Line Video for play
    - 멈추고 재생하는 것이 가능.
    - muted 된 상태가 default

--- ez.ai/client/src/components/ChatbotBuild/Preview/LinePreview/BasicPreview/VideoPreview.js
+++ ez.ai/client/src/components/ChatbotBuild/Preview/LinePreview/BasicPreview/VideoPreview.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import whitePlay from '../../../../../objects/play-circle.png';
 import blackPlay from "../../../../../objects/play-circle-black.png";
 
@@ -8,10 +8,29 @@ const VideoPreview = ({
   setClickedMainInput,
   now,
   setNow,
-
   onDelete,
   changeAvailableIcon
 }) => {
+  const lineVideo = useRef(null);
+  const [currentPlayState, setCurrentPlayState] = useState(false);
+
+  const clickHandler = () => {
+    setCurrentPlayState(!currentPlayState);
+
+    if(lineVideo.current !== null) { // 객체 null 예외처리
+      if(!currentPlayState){ // 상태 변화 전 pause였으면
+        lineVideo.current.play(); //오디오 플레이
+      }
+      else { // 상태 변화 전 play 였으면
+        lineVideo.current.pause();
+      }
+    }
+  }
+
+  useEffect(() => {
+    setCurrentPlayState(false);
+  }, [v.content]);
+  
   return(
       <div className="main-preview">
         <div
@@ -26,24 +45,38 @@ const VideoPreview = ({
         >
           {v.content != "" ?
             <>
-              <div className="video-content-line">
-                <video src={v.content} autoplay loop/>
-              </div>
-              <img className="video-play-icon" src={whitePlay}></img>
+              {currentPlayState ?
+                <div className="video-playstate-line">
+                  <video src={v.content} muted autoPlay="true" controls="true" 
+                         ref={lineVideo}
+                         onPause={(e) => setCurrentPlayState(false)}
+                  />
+                </div>
+              :
+                <>
+                  <div className="video-content-line">
+                    <video src={v.content} ref={lineVideo}/>
+                  </div>
+                  <img className="video-play-icon" 
+                       src={whitePlay}
+                       onClick={clickHandler}>
+                  </img>
+                </>
+              }
             </>
             :
             <img className="video-default-icon" src={blackPlay}></img>
           }
         </div>
-      <div
-        className="tool-delete delete-video"
-        onClick={() => {
-          onDelete(v.id);
-        }}
-      >
-        <i className="fas fa-times"></i>
+        <div
+          className="tool-delete delete-video"
+          onClick={() => {
+            onDelete(v.id);
+          }}
+        >
+          <i className="fas fa-times"></i>
+        </div>
       </div>
-    </div>
   );
 };
 
