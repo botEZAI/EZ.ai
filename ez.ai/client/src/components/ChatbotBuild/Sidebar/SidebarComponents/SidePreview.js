1b3ce60084e18f5fc8b1e24535abd21366af6847
ChayanRin <k852012@naver.com>
2020-08-25 00:28:48 +0900

    텔레그램 inner outer 시간 정보 추가

--- ez.ai/client/src/components/ChatbotBuild/Sidebar/SidebarComponents/SidePreview.js
+++ ez.ai/client/src/components/ChatbotBuild/Sidebar/SidebarComponents/SidePreview.js
@@ -139,15 +139,18 @@ const SidePreview = (props) => {
                           : { height: "100%" }
                       }
                     />
+                    {currentTime("inner")}
                   </div>
                ) : null}
               {c.content.title !== "" ? (
                 <div className="preview-receive buttons-title-telegram">
                   {c.content.title}
+                  {currentTime("outer")}
                 </div>
               ) : null }
               <div className="preview-receive buttons-text-telegram">
                {c.content.text !== "" ? c.content.text : "text"}
+                {currentTime("outer")}
               </div>
               {setKeyboard("platform-telegram-buttons")}
               {setFixedMenu(c.content.actions)}
